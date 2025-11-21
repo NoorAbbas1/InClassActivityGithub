f89b3b2de4527a3b4c5f67ac3db9d3a40f320dc9
Noor <107961296+NoorAbbas1@users.noreply.github.com>
2025-11-21 12:46:11 -0500

    main.py
    Our lab!

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 # InClassActivityGithub
-From Noor and Dareen, for Python class.
+# From Noor and Dareen, for Python class.
 
 # Simple collaboration file for our lab
 
-def hello_from_dareen();
-    return "Hello from Dareen!" #Edited by Dareen
+def hello_from_dareen():
+    return "Hello from Dareen!"  # Edited by Dareen
 
-def hello_from_noor();
-    return "Hello from Noor!" #Edited by Noor
+def hello_from_noor():
+    return "Hello from Noor!"  # Edited by Noor
 
-def add_numbers (a, b);
+def add_numbers(a, b):
     return a + b
 
-print (hello_from_dareen())
-print (hello_from_noor())
-print("2 + 3 =, add_numbers(2, 3))
-
+print(hello_from_dareen())
+print(hello_from_noor())
+print("2 + 3 =", add_numbers(2, 3))
